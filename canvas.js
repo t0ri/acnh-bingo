2febdecc37fc3f34cf8bf15d0b4a94f190e1cf1f
t0ri <victoriarosemurray@gmail.com>
2020-11-17 15:45:32 -0800

    ux revamp

--- canvas.js
+++ canvas.js
@@ -21,31 +21,31 @@ const iconNums = {
     }
   },
   gridPositions: {
-    1: ['B', '1'],
-    2: ['I', '1'],
-    3: ['N', '1'],
-    4: ['G', '1'],
-    5: ['O', '1'],
-    6: ['B', '2'],
-    7: ['I', '2'],
-    8: ['N', '2'],
-    9: ['G', '2'],
-    10: ['O', '2'],
-    11: ['B', '3'],
-    12: ['I', '3'],
-    13: ['N', '3'],
-    14: ['G', '3'],
-    15: ['O', '3'],
-    16: ['B', '4'],
-    17: ['I', '4'],
-    18: ['N', '4'],
-    19: ['G', '4'],
-    20: ['O', '4'],
-    21: ['B', '5'],
-    22: ['I', '5'],
-    23: ['N', '5'],
-    24: ['G', '5'],
-    25: ['O', '5']
+    'B 1': 1,
+    'I 1': 2, 
+    'N 1': 3, 
+    'G 1': 4, 
+    'O 1': 5, 
+    'B 2': 6, 
+    'I 2': 7, 
+    'N 2': 8, 
+    'G 2': 9, 
+    'O 2': 10, 
+    'B 3': 11, 
+    'I 3': 12, 
+    'N 3': 13, 
+    'G 3': 14, 
+    'O 3': 15, 
+    'B 4': 16, 
+    'I 4': 17, 
+    'N 4': 18, 
+    'G 4': 19, 
+    'O 4': 20, 
+    'B 5': 21, 
+    'I 5': 22, 
+    'N 5': 23, 
+    'G 5': 24, 
+    'O 5': 25, 
   }
 }
 
@@ -103,11 +103,23 @@ const detectSlot = (windowX, windowY) => {
   const column = detectColumn(windowX - offsetLeft - 8)
   const row = detectRow(windowY - offsetTop - 20)
 
-  console.info(column, row)
+  return `${column} ${row}`
 }
 
+let gridPosition
+let slot
+
 const clickCanvas = (e) => {
-  detectSlot(e.clientX, e.clientY)
+  slot = detectSlot(e.clientX, e.clientY)
+  gridPosition = iconNums.gridPositions[slot]
+
+  if (gridPosition && gridPosition !== 13) {
+    const instructions = document.getElementById('instructions')
+    instructions.innerHTML = `Select a Villager to fill ${slot.replace(/\s/g, '')}.`
+  } else if (gridPosition === 13) {
+    const instructions = document.getElementById('instructions')
+    instructions.innerHTML = `Cannot Replace Free Space!`
+  }
 }
 
 // Set Background
@@ -122,27 +134,35 @@ const setBackground = (() => {
 
 // Set Villager
 // `updateVillager('ant01', 'My Name', 1)`
-const updateVillager = (villagerId, villagerName, selectionNum) => {
-  let icon = new Image()
-  icon.src = `https://acnhapi.com/v1/images/villagers/${villagerId}`
-  icon.crossOrigin = 'anonymous'
-  icon.onload = () => {
-    const column = iconNums.gridPositions[selectionNum][0]
-    const row = iconNums.gridPositions[selectionNum][1]
+const updateVillager = (villagerId, villagerName) => {
+  if (gridPosition && gridPosition !== 13) {
+    let icon = new Image()
+    icon.src = `https://acnhapi.com/v1/images/villagers/${villagerId}`
+    icon.crossOrigin = 'anonymous'
     
-    const columnCenter = iconNums.slots.columns[column] + 30 + 20
-    const textWidth = Number(ctx.measureText(villagerName).width) / 2
-    const textPosition = iconNums.slots.rows[row] + iconNums.height + 30
+    let column = slot[0]
+    let row = slot[2]
     
-    ctx.fillStyle = "#fff"
-    ctx.fillRect(iconNums.slots.columns[column] + 20, iconNums.slots.rows[row] + 15, iconNums.width, iconNums.height + 20)
-
-    ctx.drawImage(icon, iconNums.slots.columns[column] + 20, iconNums.slots.rows[row] + 15, iconNums.width, iconNums.height)
-
-    ctx.font = '14px Balsamiq Sans'
-    ctx.fillStyle = '#60bec3'
-    ctx.fillText(villagerName, columnCenter - textWidth, textPosition)
+    icon.onload = () => {
+      const columnCenter = iconNums.slots.columns[column] + 30 + 20
+      const textWidth = Number(ctx.measureText(villagerName).width) / 2
+      const textPosition = iconNums.slots.rows[row] + iconNums.height + 30
+      
+      ctx.fillStyle = "#fff"
+      ctx.fillRect(iconNums.slots.columns[column] + 20, iconNums.slots.rows[row] + 15, iconNums.width, iconNums.height + 20)
+  
+      ctx.drawImage(icon, iconNums.slots.columns[column] + 20, iconNums.slots.rows[row] + 15, iconNums.width, iconNums.height)
+  
+      ctx.font = '14px Balsamiq Sans'
+      ctx.fillStyle = '#60bec3'
+      ctx.fillText(villagerName, columnCenter - textWidth, textPosition)
+    }
   }
+
+  gridPosition = undefined
+  slot = undefined
+  const instructions = document.getElementById('instructions')
+  instructions.innerHTML = 'Select a Spot on the Bingo Card to Fill.'
 }
 
 // Download Button Clicked
--- selector.js
+++ selector.js
@@ -1,5 +1,5 @@
 const api = 'https://acnhapi.com/v1/villagers/'
-const villagers = []
+let villagers = []
 
 const getVillagers = async (url) => {
   const response = await fetch(url)
@@ -9,56 +9,236 @@ const getVillagers = async (url) => {
   })
 }
 
-const createVillagerSelector = () => {
-  // Get Elements
-  let selectionSection = document.getElementById('selection')
-  let selectorElement = document.querySelector('.villager-select')
-  
-  // Set First Selection View
-  selectorElement.innerHTML = '<option value="">&nbsp; &nbsp;Select Villager</option>'
-  
-  // Append an `<option>` for each villager
-  villagers.forEach((villager) => {
-    const option = document.createElement('option')
-    option.value = villager[0]
-    option.innerHTML = villager[1]['name']['name-USen']
-    selectorElement.appendChild(option)
-  })
+const sortVillagers = () => {
+  let anteaters = villagers.filter(villager => villager[1].species === 'Anteater')
+  let bears = villagers.filter(villager => villager[1].species === 'Bear')
+  let birds = villagers.filter(villager => villager[1].species === 'Bird')
+  let chickens = villagers.filter(villager => villager[1].species === 'Chicken')
+  let cows = villagers.filter(villager => villager[1].species === 'Cow')
+  let gators = villagers.filter(villager => villager[1].species === 'Alligator')
+  let deer = villagers.filter(villager => villager[1].species === 'Deer')
+  let dogs = villagers.filter(villager => villager[1].species === 'Dog')
+  let ducks = villagers.filter(villager => villager[1].species === 'Duck')
+  let eagles = villagers.filter(villager => villager[1].species === 'Eagle')
+  let elephants = villagers.filter(villager => villager[1].species === 'Elephant')
+  let frogs = villagers.filter(villager => villager[1].species === 'Frog')
+  let goats = villagers.filter(villager => villager[1].species === 'Goat')
+  let gorillas = villagers.filter(villager => villager[1].species === 'Gorilla')
+  let hamsters = villagers.filter(villager => villager[1].species === 'Hamster')
+  let hippos = villagers.filter(villager => villager[1].species === 'Hippo')
+  let horses = villagers.filter(villager => villager[1].species === 'Horses')
+  let kangaroos = villagers.filter(villager => villager[1].species === 'Kangaroo')
+  let koalas = villagers.filter(villager => villager[1].species === 'Koala')
+  let lions = villagers.filter(villager => villager[1].species === 'Lion')
+  let monkeys = villagers.filter(villager => villager[1].species === 'Monkey')
+  let mice = villagers.filter(villager => villager[1].species === 'Mouse')
+  let octopuses = villagers.filter(villager => villager[1].species === 'Octopus')
+  let ostriches = villagers.filter(villager => villager[1].species === 'Ostrich')
+  let penguins = villagers.filter(villager => villager[1].species === 'Penguin')
+  let pigs = villagers.filter(villager => villager[1].species === 'Pig')
+  let rabbits = villagers.filter(villager => villager[1].species === 'Rabbit')
+  let rhinos = villagers.filter(villager => villager[1].species === 'Rhino')
+  let sheep = villagers.filter(villager => villager[1].species === 'Sheep')
+  let squirrels = villagers.filter(villager => villager[1].species === 'Squirrel')
+  let tigers = villagers.filter(villager => villager[1].species === 'Tiger')
+  let wolves = villagers.filter(villager => villager[1].species === 'Wolf')
 
-  // Create 24 Select Elements
-  for (let num = 2; num <= 25; num++) {
-    if (num === 13) {
-      const select = document.createElement('select')
-      const option = document.createElement('option')
-      option.innerHTML = ' &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; FREE!'
-      select.appendChild(option)
-      selectionSection.appendChild(select)
-    } else {
-      let clone = selectorElement.cloneNode(true)
-      clone.name = `${num}`
-      selectionSection.appendChild(clone)
-    }
+  return villagerTypes = {
+    anteaters: {
+      animalName: 'Anteaters',
+      anteaters,
+    },
+    bears: {
+      animalName: 'Bears',
+      bears,
+    },
+    birds: {
+      animalName: 'Birds',
+      birds,
+    },
+    chickens: {
+      animalName: 'Chickens',
+      chickens,
+    },
+    cows: {
+      animalName: 'Cows',
+      cows,
+    },
+    gators: {
+      animalName: 'Alligators and Crocodiles',
+      gators,
+    },
+    deer: {
+      animalName: 'Deer',
+      deer,
+    },
+    dogs: {
+      animalName: 'Dogs',
+      dogs,
+    },
+    ducks: {
+      animalName: 'Ducks',
+      ducks,
+    },
+    eagles: {
+      animalName: 'Eagles',
+      eagles,
+    },
+    elephants: {
+      animalName: 'Elephants',
+      elephants,
+    },
+    frogs: {
+      animalName: 'Frogs',
+      frogs,
+    },
+    goats: {
+      animalName: 'Goats',
+      goats,
+    },
+    gorillas: {
+      animalName: 'Gorillas',
+      gorillas,
+    },
+    hamsters: {
+      animalName: 'Hamsters',
+      hamsters,
+    },
+    hippos: {
+      animalName: 'Hippos',
+      hippos,
+    },
+    horses: {
+      animalName: 'Horses',
+      horses,
+    },
+    kangaroos: {
+      animalName: 'Kangaroos',
+      kangaroos,
+    },
+    koalas: {
+      animalName: 'Koalas',
+      koalas,
+    },
+    lions: {
+      animalName: 'Lions',
+      lions,
+    },
+    monkeys: {
+      animalName: 'Monkeys',
+      monkeys,
+    },
+    mice: {
+      animalName: 'Mice',
+      mice,
+    },
+    octopuses: {
+      animalName: 'Octopuses',
+      octopuses,
+    },
+    ostriches: {
+      animalName: 'Ostriches',
+      ostriches,
+    },
+    penguins: {
+      animalName: 'Penguins',
+      penguins,
+    },
+    pigs: {
+      animalName: 'Pigs',
+      pigs,
+    },
+    rabbits: {
+      animalName: 'Rabbits',
+      rabbits,
+    },
+    rhinos: {
+      animalName: 'Rhinos',
+      rhinos,
+    },
+    sheep: {
+      animalName: 'Sheep',
+      sheep,
+    },
+    squirrels: {
+      animalName: 'Squirrels',
+      squirrels,
+    },
+    tigers: {
+      animalName: 'Tigers',
+      tigers,
+    },
+    wolves: {
+      animalName: 'Wolves',
+      wolves,
+    },
+  }
+}
+
+const eventListeners = () => {
+  // Get Elements
+  let selectorsElements = document.querySelectorAll('.animal-type-portrait-container')
+  selectorsElements.forEach((selectElement) => {
+    selectElement.addEventListener('click', event => {
+      let element = event.target
+      while (element.className !== 'portrait-container') {
+        element = element.parentNode
+      }
+      updateVillager(element.attributes[1]['nodeValue'], element.attributes[2]['nodeValue'])
+    })
+  })
+}
+
+const createSelections = () => {
+  // Create Animal Type Container
+  const villagersContainer = document.getElementById('villagers-container')
+  villagersContainer.innerHTML = ''
+  for (const animalType in villagers) {
+    // Create Animal Type Header
+    const animalHeading = document.createElement('h2')
+    animalHeading.innerHTML = villagers[animalType]['animalName']
+    animalHeading.className = 'animal-type-header'
+    villagersContainer.appendChild(animalHeading)
+
+    // Create Animal Type Portraits Container
+    const animalContainer = document.createElement('div')
+    animalContainer.className = 'animal-type-portrait-container'
+
+    const animals = villagers[animalType][animalType].filter((value, index, array)=>array.findIndex((index) => (index[0] === value[0])) === index)
+
+    // Create Portraits
+    animals.map((animal) => {
+      // Create Portrait Container
+      const animalPortraitContainer = document.createElement('div')
+      animalPortraitContainer.className = `portrait-container`
+      animalPortraitContainer.setAttribute('data-id', animal[0])
+      animalPortraitContainer.setAttribute('data-name', animal[1]['name']['name-USen'])
+
+      // Create Portrait
+      const animalPortrait = document.createElement('img')
+      animalPortrait.src = animal[1]['icon_uri']
+      animalPortrait.className = 'animal-portrait grow'
+
+      // Create Animal Name Text
+      const animalName = document.createElement('p')
+      animalName.className = 'animal-name'
+      animalName.innerHTML = animal[1]['name']['name-USen']
+
+      animalPortraitContainer.appendChild(animalPortrait)
+      animalPortraitContainer.appendChild(animalName)
+      animalContainer.appendChild(animalPortraitContainer)
+    })
+
+    villagersContainer.appendChild(animalContainer)
   }
 
   eventListeners()
 }
 
-const eventListeners = () => {
-  // Get Elements
-  let selectorsElements = document.querySelectorAll('.villager-select')
-  selectorsElements.forEach((selectElement) => {
-    selectElement.addEventListener('input', event => {
-      const villagerId = event.target.selectedOptions[0].value
-      const villagerName = event.target.selectedOptions[0].innerHTML
-      let selectorId = event.target.name
-      updateVillager(villagerId, villagerName, selectorId)
-    })
-  })
-}
-
 const init = () => {
-  if (villagers.length >= 391) {
-    createVillagerSelector()
+  if (villagers.length >= 393) {
+    villagers = sortVillagers()
+    createSelections()
   } else {
     getVillagers(api)
     setTimeout(init, 500)
